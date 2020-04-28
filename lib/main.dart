82a00c4330792dd7353ce7b231d8f6eb40b7e4c2
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-04-28 16:22:08 +0530

    Disabled orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:store_app/screens/auth_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
