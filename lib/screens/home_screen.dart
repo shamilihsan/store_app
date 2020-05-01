4f34c7ad3916d04183fc016a782215ccaae04044
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-01 17:32:00 +0530

    Added spacing at bottom

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -94,7 +94,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 padding: EdgeInsets.only(left: 25.0, right: 20.0),
                 children: <Widget>[
                   Padding(
-                    padding: EdgeInsets.only(top: 45.0),
+                    padding: EdgeInsets.only(top: 45.0, bottom: 10.0),
                     child: Container(
                       height: mediaQuery.size.height - 300.0,
                       child: ListView(
