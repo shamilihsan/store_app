6d48b2ec54e39921e52fac2283810d95883436c5
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-01 17:41:29 +0530

    Removed extra listview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -89,70 +89,74 @@ class _HomeScreenState extends State<HomeScreen> {
                   topLeft: Radius.circular(75.0),
                 ),
               ),
-              child: ListView(
-                primary: false,
-                padding: EdgeInsets.only(left: 25.0, right: 20.0),
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 45.0, bottom: 10.0),
-                    child: Container(
-                      height: mediaQuery.size.height - 300.0,
-                      child: ListView(
-                        children: <Widget>[_buildItem()],
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 45.0, bottom: 10.0),
+                      child: Container(
+                        height: mediaQuery.size.height - 300.0,
+                        child: ListView(
+                          children: <Widget>[_buildItem()],
+                        ),
                       ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Container(
-                        height: 65.0,
-                        width: 60.0,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            style: BorderStyle.solid,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Center(
-                          child: Icon(Icons.search, color: Colors.black),
-                        ),
-                      ),
-                      Container(
-                        height: 65.0,
-                        width: 60.0,
-                        decoration: BoxDecoration(
-                          border: Border.all(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                          height: 65.0,
+                          width: 60.0,
+                          decoration: BoxDecoration(
+                            border: Border.all(
                               color: Colors.grey,
                               style: BorderStyle.solid,
-                              width: 1.0),
-                          borderRadius: BorderRadius.circular(10.0),
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Center(
+                            child: Icon(Icons.search, color: Colors.black),
+                          ),
                         ),
-                        child: Center(
-                          child:
-                              Icon(Icons.shopping_basket, color: Colors.black),
-                        ),
-                      ),
-                      Container(
-                        height: 65.0,
-                        width: 120.0,
-                        decoration: BoxDecoration(
+                        Container(
+                          height: 65.0,
+                          width: 60.0,
+                          decoration: BoxDecoration(
                             border: Border.all(
                                 color: Colors.grey,
                                 style: BorderStyle.solid,
                                 width: 1.0),
                             borderRadius: BorderRadius.circular(10.0),
-                            color: Theme.of(context).accentColor),
-                        child: Center(
-                            child: Text('Checkout',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 15.0))),
-                      )
-                    ],
-                  )
-                ],
+                          ),
+                          child: Center(
+                            child: Icon(Icons.shopping_basket,
+                                color: Colors.black),
+                          ),
+                        ),
+                        Container(
+                          height: 65.0,
+                          width: 120.0,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.grey,
+                                  style: BorderStyle.solid,
+                                  width: 1.0),
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Theme.of(context).accentColor),
+                          child: Center(
+                            child: Text(
+                              'Checkout',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 15.0),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           )
