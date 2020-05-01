9756777af35c2ee72e33697870ed0010e3b44ed3
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-01 19:05:59 +0530

    Code optimization

--- lib/screens/item_detail_screen.dart
+++ lib/screens/item_detail_screen.dart
@@ -53,165 +53,173 @@ class _ItemDetailsScreenState extends State<ItemDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).accentColor,
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: Icon(Icons.arrow_back_ios),
-            color: Colors.white,
-          ),
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          title: Text(
-            'Details',
-            style: TextStyle(fontSize: 18.0, color: Colors.white),
-          ),
-          centerTitle: true,
+      backgroundColor: Theme.of(context).accentColor,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.white,
         ),
-        body: ListView(children: [
-          Stack(children: [
-            Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.transparent),
-            Positioned(
-                top: 75.0,
-                child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(45.0),
-                          topRight: Radius.circular(45.0),
-                        ),
-                        color: Colors.white),
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width)),
-            Positioned(
-              top: 30.0,
-              left: (MediaQuery.of(context).size.width / 2) - 100.0,
-              child: Hero(
-                tag: widget.heroTag,
-                child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(widget.imageUrl),
-                          fit: BoxFit.cover),
-                    ),
-                    height: 200.0,
-                    width: 200.0),
-              ),
-            ),
-            Positioned(
-                top: 250.0,
-                left: 25.0,
-                right: 25.0,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(widget.itemName,
-                        style: TextStyle(
-                            fontSize: 22.0, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 20.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(widget.itemPrice.toString(),
-                            style:
-                                TextStyle(fontSize: 20.0, color: Colors.grey)),
-                        Container(height: 25.0, color: Colors.grey, width: 1.0),
-                        Container(
-                          width: 125.0,
-                          height: 40.0,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(17.0),
-                              color: Theme.of(context).accentColor),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              InkWell(
-                                onTap: () => _decreaseQuantity(),
-                                child: Container(
-                                  height: 25.0,
-                                  width: 25.0,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(7.0),
-                                      color: Theme.of(context).accentColor),
-                                  child: Center(
-                                    child: Icon(
-                                      Icons.remove,
-                                      color: Colors.white,
-                                      size: 20.0,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Text(numberOfItems.toString(),
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 15.0)),
-                              InkWell(
-                                onTap: () => _increaseQuantity(),
-                                child: Container(
-                                  height: 25.0,
-                                  width: 25.0,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(7.0),
-                                      color: Colors.white),
-                                  child: Center(
-                                    child: Icon(
-                                      Icons.add,
-                                      color: Theme.of(context).accentColor,
-                                      size: 20.0,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text(
+          'Details',
+          style: TextStyle(fontSize: 18.0, color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.transparent),
+              Positioned(
+                  top: 75.0,
+                  child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(45.0),
+                            topRight: Radius.circular(45.0),
                           ),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 20.0),
-                    Container(
-                        height: 150.0,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: <Widget>[
-                            _buildInfoCard('WEIGHT', '300', 'G'),
-                            SizedBox(width: 10.0),
-                            _buildInfoCard('CALORIES', '267', 'CAL'),
-                            SizedBox(width: 10.0),
-                            _buildInfoCard('VITAMINS', 'A, B6', 'VIT'),
-                            SizedBox(width: 10.0),
-                            _buildInfoCard('AVAIL', 'NO', 'AV')
-                          ],
-                        )),
-                    SizedBox(height: 20.0),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 5.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10.0),
-                                topRight: Radius.circular(10.0),
-                                bottomLeft: Radius.circular(25.0),
-                                bottomRight: Radius.circular(25.0)),
-                            color: Colors.black),
-                        height: 50.0,
-                        child: Center(
-                          child: Text(
-                            'Rs. ${total.toString()}',
-                            style: TextStyle(
-                              color: Colors.white,
+                          color: Colors.white),
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width)),
+              Positioned(
+                top: 30.0,
+                left: (MediaQuery.of(context).size.width / 2) - 100.0,
+                child: Hero(
+                  tag: widget.heroTag,
+                  child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(widget.imageUrl),
+                            fit: BoxFit.cover),
+                      ),
+                      height: 200.0,
+                      width: 200.0),
+                ),
+              ),
+              Positioned(
+                  top: 250.0,
+                  left: 25.0,
+                  right: 25.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(widget.itemName,
+                          style: TextStyle(
+                              fontSize: 22.0, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 20.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(widget.itemPrice.toString(),
+                              style: TextStyle(
+                                  fontSize: 20.0, color: Colors.grey)),
+                          Container(
+                              height: 25.0, color: Colors.grey, width: 1.0),
+                          Container(
+                            width: 125.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(17.0),
+                                color: Theme.of(context).accentColor),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                InkWell(
+                                  onTap: () => _decreaseQuantity(),
+                                  child: Container(
+                                    height: 25.0,
+                                    width: 25.0,
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(7.0),
+                                        color: Theme.of(context).accentColor),
+                                    child: Center(
+                                      child: Icon(
+                                        Icons.remove,
+                                        color: Colors.white,
+                                        size: 20.0,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Text(numberOfItems.toString(),
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15.0)),
+                                InkWell(
+                                  onTap: () => _increaseQuantity(),
+                                  child: Container(
+                                    height: 25.0,
+                                    width: 25.0,
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(7.0),
+                                        color: Colors.white),
+                                    child: Center(
+                                      child: Icon(
+                                        Icons.add,
+                                        color: Theme.of(context).accentColor,
+                                        size: 20.0,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 20.0),
+                      Container(
+                          height: 150.0,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: <Widget>[
+                              _buildInfoCard('WEIGHT', '300', 'G'),
+                              SizedBox(width: 10.0),
+                              _buildInfoCard('CALORIES', '267', 'CAL'),
+                              SizedBox(width: 10.0),
+                              _buildInfoCard('VITAMINS', 'A, B6', 'VIT'),
+                              SizedBox(width: 10.0),
+                              _buildInfoCard('AVAIL', 'NO', 'AV')
+                            ],
+                          )),
+                      SizedBox(height: 20.0),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 5.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10.0),
+                                  topRight: Radius.circular(10.0),
+                                  bottomLeft: Radius.circular(25.0),
+                                  bottomRight: Radius.circular(25.0)),
+                              color: Colors.black),
+                          height: 50.0,
+                          child: Center(
+                            child: Text(
+                              'Rs. ${total.toString()}',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    )
-                  ],
-                ))
-          ])
-        ]));
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildInfoCard(String cardTitle, String info, String unit) {
