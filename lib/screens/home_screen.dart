18a27d1cb24a8f5fe891456b1140a398e8213dc3
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-01 17:05:13 +0530

    Added Flexible widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,80 +22,84 @@ class _HomeScreenState extends State<HomeScreen> {
       drawer: AppDrawer(),
       body: Column(
         children: <Widget>[
-          Container(
-            height: (mediaQuery.size.height) * 0.3,
-            padding: EdgeInsets.only(top: 15.0, left: 10.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Container(
-                      width: 125.0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          IconButton(
-                            icon: Icon(Icons.filter_list),
-                            color: Colors.white,
-                            onPressed: () {},
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.menu),
-                            color: Colors.white,
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 25.0),
-                Container(
-                  padding: EdgeInsets.only(left: 40.0),
-                  child: Row(
+          Flexible(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.only(top: 15.0, left: 10.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
-                      Text(
-                        'Our',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25.0,
+                      Container(
+                        width: 125.0,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.filter_list),
+                              color: Colors.white,
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.menu),
+                              color: Colors.white,
+                              onPressed: () {},
+                            ),
+                          ],
                         ),
-                      ),
-                      SizedBox(width: 10.0),
-                      Text(
-                        'Menu',
-                        style: TextStyle(color: Colors.white, fontSize: 25.0),
                       )
                     ],
                   ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            height: (mediaQuery.size.height) * 0.7,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(75.0),
-              ),
-            ),
-            child: ListView(
-              primary: false,
-              padding: EdgeInsets.only(left: 25.0, right: 20.0),
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 45.0),
-                  child: Container(
-                    height: mediaQuery.size.height - 300.0,
-                    child: ListView(
-                      children: <Widget>[_buildItem()],
+                  SizedBox(height: 25.0),
+                  Container(
+                    padding: EdgeInsets.only(left: 40.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Our',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        Text(
+                          'Menu',
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        )
+                      ],
                     ),
                   ),
-                )
-              ],
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(75.0),
+                ),
+              ),
+              child: ListView(
+                primary: false,
+                padding: EdgeInsets.only(left: 25.0, right: 20.0),
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 45.0),
+                    child: Container(
+                      height: mediaQuery.size.height - 300.0,
+                      child: ListView(
+                        children: <Widget>[_buildItem()],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         ],
