a33811023c789a9fc807b160558adda06ed38c49
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-02 16:16:21 +0530

    Improved home_screen ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -94,90 +94,96 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               child: Padding(
-                padding: const EdgeInsets.only(left: 25.0, right: 20.0),
+                padding:
+                    const EdgeInsets.only(top: 20, left: 25.0, right: 20.0),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(top: 45.0, bottom: 10.0),
-                      child: Container(
-                          height: mediaQuery.size.height - 300.0,
-                          child: ItemList()),
+                    Flexible(
+                      flex: 4,
+                      child: Container(child: ItemList()),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Container(
-                          height: 65.0,
-                          width: 60.0,
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.grey,
-                              style: BorderStyle.solid,
-                              width: 1.0,
-                            ),
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: Center(
-                            child: Icon(Icons.search, color: Colors.black),
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () => Navigator.of(context)
-                              .pushNamed(CartScreen.routeName),
-                          child: Container(
-                            height: 65.0,
-                            width: 60.0,
-                            decoration: BoxDecoration(
-                              border: Border.all(
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        width: mediaQuery.size.width - 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Container(
+                              height: 65.0,
+                              width: 60.0,
+                              decoration: BoxDecoration(
+                                border: Border.all(
                                   color: Colors.grey,
                                   style: BorderStyle.solid,
-                                  width: 1.0),
-                              borderRadius: BorderRadius.circular(10.0),
+                                  width: 1.0,
+                                ),
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Center(
+                                child: Icon(Icons.search, color: Colors.black),
+                              ),
                             ),
-                            child: Column(
-                              children: <Widget>[
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
+                            InkWell(
+                              onTap: () => Navigator.of(context)
+                                  .pushNamed(CartScreen.routeName),
+                              child: Container(
+                                height: 65.0,
+                                width: 60.0,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Colors.grey,
+                                      style: BorderStyle.solid,
+                                      width: 1.0),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                child: Column(
                                   children: <Widget>[
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          right: 5, top: 3),
-                                      child: Consumer<Cart>(
-                                        builder: (_, cartData, ch) =>
-                                            Text(cartData.itemCount.toString()),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              right: 5, top: 3),
+                                          child: Consumer<Cart>(
+                                            builder: (_, cartData, ch) => Text(
+                                                cartData.itemCount.toString()),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Center(
+                                      child: Icon(
+                                        Icons.shopping_basket,
+                                        color: Colors.black,
                                       ),
                                     ),
                                   ],
                                 ),
-                                Center(
-                                  child: Icon(
-                                    Icons.shopping_basket,
-                                    color: Colors.black,
-                                  ),
+                              ),
+                            ),
+                            Container(
+                              height: 65.0,
+                              width: 120.0,
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Colors.grey,
+                                      style: BorderStyle.solid,
+                                      width: 1.0),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: Theme.of(context).accentColor),
+                              child: Center(
+                                child: Text(
+                                  'Checkout',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 15.0),
                                 ),
-                              ],
-                            ),
-                          ),
+                              ),
+                            )
+                          ],
                         ),
-                        Container(
-                          height: 65.0,
-                          width: 120.0,
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Colors.grey,
-                                  style: BorderStyle.solid,
-                                  width: 1.0),
-                              borderRadius: BorderRadius.circular(10.0),
-                              color: Theme.of(context).accentColor),
-                          child: Center(
-                            child: Text(
-                              'Checkout',
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 15.0),
-                            ),
-                          ),
-                        )
-                      ],
+                      ),
                     )
                   ],
                 ),
