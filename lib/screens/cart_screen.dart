ed8f02f01a374bc2a615327db41bf4d661035aed
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-02 14:33:52 +0530

    Revamped cart screen again

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -27,25 +27,58 @@ class _CartScreenState extends State<CartScreen> {
         elevation: 0.0,
         centerTitle: true,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(75.0),
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 25.0, right: 20.0),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 45.0, bottom: 10.0),
-                child: Container(
-                    height: mediaQuery.size.height - 300.0, child: null),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.only(top: mediaQuery.padding.top),
+              child: Column(
+                children: <Widget>[
+                  const SizedBox(height: 25.0),
+                  Container(
+                    padding: EdgeInsets.only(left: 40.0),
+                    child: Row(
+                      children: <Widget>[
+                        const Text(
+                          'Your',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        const Text(
+                          'Cart',
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          Flexible(
+            flex: 3,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(75.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 25.0),
+                child: Column(
+                  children: <Widget>[],
+                ),
+              ),
+            ),
+          ),
+        ],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
       ),
     );
   }
