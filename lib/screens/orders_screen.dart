446d157ceec4e39d512c43b7a07702479bd6347e
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-07 14:12:50 +0530

    Structered basic order screen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/widgets/app_drawer.dart';
 
 class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Orders'),
+    final mediaQuery = MediaQuery.of(context);
+    final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Theme.of(context).primaryColor,
+      drawer: AppDrawer(),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.only(top: mediaQuery.padding.top),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        width: mediaQuery.size.width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.menu),
+                              color: Colors.white,
+                              onPressed: () =>
+                                  _scaffoldKey.currentState.openDrawer(),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.only(left: 40.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Your',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        Text(
+                          'Orders',
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: Text('data'),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
