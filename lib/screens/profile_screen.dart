b47323f94b4ab049395e236bc1e4114321ff15c1
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-16 15:28:02 +0530

    Added styling to screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -6,10 +6,92 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       drawer: AppDrawer(),
-      body: Center(
-        child: Text('Profile Screen'),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              height: mediaQuery.size.height * 1 / 4,
+              padding: EdgeInsets.only(top: mediaQuery.padding.top),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        width: mediaQuery.size.width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.arrow_back_ios),
+                              color: Colors.white,
+                              onPressed: () => Navigator.of(context).pop(),
+                              // onPressed: () =>
+                              //     _scaffoldKey.currentState.openDrawer(),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.only(left: 40.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Your',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        Text(
+                          'Profile',
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.only(top: 50),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(75.0),
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: mediaQuery.size.width,
+                      height: mediaQuery.size.height * 3 / 4 - 50,
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'E-Mail'),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value.isEmpty || !value.contains('@')) {
+                            return 'Invalid email!';
+                          }
+                        },
+                        onSaved: (value) {},
+                      ),
+                    )
+                  ],
+                )),
+          ],
+        ),
       ),
     );
   }
