9329c48bf044f578b584f7b109b316800529ad0b
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-16 16:11:09 +0530

    Updated UI to accomadate keyboard

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -11,59 +11,60 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       drawer: AppDrawer(),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Container(
-            height: mediaQuery.size.height * 1 / 4,
-            padding: EdgeInsets.only(top: mediaQuery.padding.top),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      width: mediaQuery.size.width,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          IconButton(
-                            icon: Icon(Icons.arrow_back_ios),
-                            color: Colors.white,
-                            onPressed: () => Navigator.of(context).pop(),
-                            // onPressed: () =>
-                            //     _scaffoldKey.currentState.openDrawer(),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  padding: EdgeInsets.only(left: 40.0),
-                  child: Row(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              height: mediaQuery.size.height * 1 / 4,
+              padding: EdgeInsets.only(top: mediaQuery.padding.top),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text(
-                        'Your',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25.0,
+                      Container(
+                        width: mediaQuery.size.width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.arrow_back_ios),
+                              color: Colors.white,
+                              onPressed: () => Navigator.of(context).pop(),
+                              // onPressed: () =>
+                              //     _scaffoldKey.currentState.openDrawer(),
+                            ),
+                          ],
                         ),
-                      ),
-                      const SizedBox(width: 10.0),
-                      Text(
-                        'Profile',
-                        style: TextStyle(color: Colors.white, fontSize: 25.0),
-                      ),
+                      )
                     ],
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.only(left: 40.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Your',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        Text(
+                          'Profile',
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Container(
+            Container(
               padding: EdgeInsets.only(top: 50),
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -71,72 +72,70 @@ class ProfileScreen extends StatelessWidget {
                   topLeft: Radius.circular(75.0),
                 ),
               ),
-              child: Column(
-                children: <Widget>[
-                  SingleChildScrollView(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      width: mediaQuery.size.width,
-                      height: mediaQuery.size.height * 3 / 4 - 50,
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            decoration: InputDecoration(labelText: 'E-Mail'),
-                            keyboardType: TextInputType.emailAddress,
-                            validator: (value) {
-                              if (value.isEmpty || !value.contains('@')) {
-                                return 'Invalid email!';
-                              }
-                            },
-                            onSaved: (value) {},
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(labelText: 'Name'),
-                            keyboardType: TextInputType.text,
-                            textCapitalization: TextCapitalization.words,
-                            validator: (value) {
-                              if (value.isEmpty || !value.contains('@')) {
-                                return 'Invalid email!';
-                              }
-                            },
-                            onSaved: (value) {},
-                          ),
-                          TextFormField(
-                            decoration:
-                                InputDecoration(labelText: 'Your Address'),
-                            textCapitalization: TextCapitalization.sentences,
-                            maxLines: 3,
-                            keyboardType: TextInputType.multiline,
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Enter your name!';
-                              }
-                              return null;
-                            },
-                            onChanged: (value) {},
-                          ),
-                          SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              RaisedButton(
-                                elevation: 3.0,
-                                textColor: Colors.white,
-                                color: Theme.of(context).accentColor,
-                                onPressed: () {},
-                                child: Text(
-                                  'Update Profile',
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                width: mediaQuery.size.width,
+                height: mediaQuery.size.height * 3 / 4 - 50,
+                child: Wrap(children: <Widget>[
+                  ListView(
+                    shrinkWrap: true,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'E-Mail'),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value.isEmpty || !value.contains('@')) {
+                            return 'Invalid email!';
+                          }
+                        },
+                        onSaved: (value) {},
                       ),
-                    ),
-                  )
-                ],
-              )),
-        ],
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Name'),
+                        keyboardType: TextInputType.text,
+                        textCapitalization: TextCapitalization.words,
+                        validator: (value) {
+                          if (value.isEmpty || !value.contains('@')) {
+                            return 'Invalid email!';
+                          }
+                        },
+                        onSaved: (value) {},
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Your Address'),
+                        textCapitalization: TextCapitalization.sentences,
+                        maxLines: 3,
+                        keyboardType: TextInputType.multiline,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Enter your name!';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {},
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          RaisedButton(
+                            elevation: 3.0,
+                            textColor: Colors.white,
+                            color: Theme.of(context).accentColor,
+                            onPressed: () {},
+                            child: Text(
+                              'Update Profile',
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ]),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
