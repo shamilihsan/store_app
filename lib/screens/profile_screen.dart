7e2dde7e25fd5c3fbcb55e74de0b1e7ae8284a0c
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-16 15:31:30 +0530

    Updated scroll view

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -11,70 +11,71 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       drawer: AppDrawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-              height: mediaQuery.size.height * 1 / 4,
-              padding: EdgeInsets.only(top: mediaQuery.padding.top),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Container(
-                        width: mediaQuery.size.width,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            IconButton(
-                              icon: Icon(Icons.arrow_back_ios),
-                              color: Colors.white,
-                              onPressed: () => Navigator.of(context).pop(),
-                              // onPressed: () =>
-                              //     _scaffoldKey.currentState.openDrawer(),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    padding: EdgeInsets.only(left: 40.0),
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          'Your',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25.0,
-                          ),
-                        ),
-                        const SizedBox(width: 10.0),
-                        Text(
-                          'Profile',
-                          style: TextStyle(color: Colors.white, fontSize: 25.0),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-                padding: EdgeInsets.only(top: 50),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(75.0),
-                  ),
-                ),
-                child: Column(
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            height: mediaQuery.size.height * 1 / 4,
+            padding: EdgeInsets.only(top: mediaQuery.padding.top),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     Container(
+                      width: mediaQuery.size.width,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(Icons.arrow_back_ios),
+                            color: Colors.white,
+                            onPressed: () => Navigator.of(context).pop(),
+                            // onPressed: () =>
+                            //     _scaffoldKey.currentState.openDrawer(),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.only(left: 40.0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        'Your',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0,
+                        ),
+                      ),
+                      const SizedBox(width: 10.0),
+                      Text(
+                        'Profile',
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.only(top: 50),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(75.0),
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  SingleChildScrollView(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
                       width: mediaQuery.size.width,
                       height: mediaQuery.size.height * 3 / 4 - 50,
                       child: TextFormField(
@@ -87,11 +88,11 @@ class ProfileScreen extends StatelessWidget {
                         },
                         onSaved: (value) {},
                       ),
-                    )
-                  ],
-                )),
-          ],
-        ),
+                    ),
+                  )
+                ],
+              )),
+        ],
       ),
     );
   }
