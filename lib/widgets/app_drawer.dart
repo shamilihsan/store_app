2a6304bd1d017c82434af8919c785b43f3c5ed93
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-05-25 14:49:14 +0530

    Updated drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -9,48 +9,56 @@ import 'package:store_app/screens/profile_screen.dart';
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
     return Drawer(
-      child: Column(
-        children: <Widget>[
-          AppBar(
-            title: Text('Welcome'),
-            automaticallyImplyLeading: false,
-          ),
-          Image.asset('assets/images/breakfast.jpg'),
-          ListTile(
-            leading: Icon(Icons.shop),
-            title: Text('Shop'),
-            onTap: () {
-              Navigator.of(context).pushReplacementNamed('/');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.payment),
-            title: Text('Orders'),
-            onTap: () {
-              Navigator.of(context).pushNamed(OrdersScreen.routeName);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.portrait),
-            title: Text('Profile'),
-            onTap: () {
-              Navigator.of(context).pushNamed(ProfileScreen.routeName);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.undo),
-            title: Text('Sign Out'),
-            onTap: () {
-              Provider.of<Cart>(context, listen: false).clear();
-              Navigator.of(context).pop();
-              // Ensures that the home route autoLogin() function is always called
-              Navigator.of(context).pushReplacementNamed('/');
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Theme.of(context).primaryColor,
+              Theme.of(context).accentColor
+            ])),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: mediaQuery.padding.top),
+            Image.asset('assets/images/welcome.png'),
+            ListTile(
+              leading: Icon(Icons.shop, color: Colors.white),
+              title: Text('Shop', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.payment, color: Colors.white),
+              title: Text('Orders', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.of(context).pushNamed(OrdersScreen.routeName);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.portrait, color: Colors.white),
+              title: Text('Profile', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Navigator.of(context).pushNamed(ProfileScreen.routeName);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.undo, color: Colors.white),
+              title: Text('Sign Out', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                Provider.of<Cart>(context, listen: false).clear();
+                Navigator.of(context).pop();
+                // Ensures that the home route autoLogin() function is always called
+                Navigator.of(context).pushReplacementNamed('/');
 
-              Provider.of<Auth>(context, listen: false).logout();
-            },
-          ),
-        ],
+                Provider.of<Auth>(context, listen: false).logout();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
